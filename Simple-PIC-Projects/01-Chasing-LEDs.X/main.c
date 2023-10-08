5c5c60b8711d1bf967546e8241dd87e66cbaa81b
Ahmad Rashed <rashed-a@web.de>
2023-10-08 17:49:52 +0200

    initial creation

---
+++ Simple-PIC-Projects/01-Chasing-LEDs.X/main.c
@@ -0,0 +1,49 @@
+/*************************************************************************************************
+                            Chasing LEDs
+                            ============
+In this project 4 LEDs are connected to PORTB of a PIC18F4520 microcontroller
+and the microcontroller is operated from a 4MHz resonator. The program turns on
+the LEDs in an anti-clockwise manner with one second delay between each output.
+The net result is that the LEDs seem to be chasing each other.
+ 
+  
+ * File:   main.c
+ * Author: Ahmad Rashed
+ * Created on 8. Oktober 2023
+***************************************************************************************************/
+
+#include <xc.h>
+
+
+#pragma config OSC = XT
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
+void delay_seconds(unsigned int seconds) {
+    //  Approximate delay (Error = 7.4e-3%/s)
+    unsigned int iterations = seconds * 10000;
+    for(unsigned int i = 0; i <iterations; i++){
+        __asm__("counter_l      equ                 0x24\n\t"
+                "movlw          0x1D\n\t"
+                "movwf          counter_l\n\t"
+                "inner:         decfsz              counter_l\n\t"
+                "bra		    inner\n\t"
+                "nop");
+    }
+}
+
+void main(void) {
+    TRISB = 0;  
+    PORTB = 0;
+    int i = 1;
+    while(1){
+        PORTB = i;
+        i = i<<1;
+        if (i == 16){
+            i = 1;
+        }
+        delay_seconds(1);
+    }
+    
+    return;
+}
